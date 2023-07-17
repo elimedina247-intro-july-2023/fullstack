bbea71bf50b2b435922defcf6fa0025fccb1d845
Eli Medina <elimedina247@gmail.com>
2023-07-17 17:10:32 -0400

    Finishing touches

--- frontend/app/src/app/app.component.ts
+++ frontend/app/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header.components';
+import { HeaderComponent } from './components/header.component';
+import { NavigationComponent } from './components/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
